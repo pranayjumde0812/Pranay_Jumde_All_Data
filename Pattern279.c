d1b61a79278a15290b1377d686e26101f45ca0c1
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2022-12-18 23:47:50 +0530

    Pattern Program279

---
+++ Pattern279.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a=65,i,j;
+	for(i=1; i<=4; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			printf("%c",a);
+		}
+		 a++;
+		printf("\n");
+	}
+	getch();
+}
