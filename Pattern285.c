abe47e086a7c54cce4c1e4aaeb8d8ebcd7e9ed03
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2022-12-19 00:01:10 +0530

    Pattern Program285

---
+++ Pattern285.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{      int i,j;
+		for (i=1;i<=4;i++)
+	{
+		for(j=69;j>=65;j--)
+		{
+			printf("%c",j);
+		}
+		
+		printf("\n");
+	}
+	getch();
+}
