847d7acae31d6e37515318b764dd562a39c77acf
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 00:22:36 +0530

    simple division in java

---
+++ Division.java
@@ -0,0 +1,11 @@
+class Division
+{
+
+   public static void main (String [] args)
+   {
+      double a=59,b=7,res;
+      res=(a/b);
+      System.out.println("The division of two numbers = "+res);
+   }
+
+}
