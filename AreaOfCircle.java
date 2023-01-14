d2b34441688123b7dbbc837c2491a55c75f547ef
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 23:54:12 +0530

    Formula Problem In Java

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+class AreaOfCircle
+{
+ public static void main(String [] args)
+ {
+  double pi=3.14;
+  int r=8;
+  double area;
+
+  area = 4 * pi *(r*r);
+
+  System.out.println("Area of circle = "+area+" cm2" );
+ }
+}
