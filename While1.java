15d87ec687f68b5197ca79768f671de0d1af8562
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 15:56:19 +0530

    While Loop In Java

---
+++ While1.java
@@ -0,0 +1,12 @@
+class While1
+{
+  public static void main(String[] args)
+  {
+   int i=0;
+   while(i<=10)
+   {
+    System.out.println(i);
+    i++;
+   }
+  }
+}
---
+++ While2.java
@@ -0,0 +1,12 @@
+class While2
+{
+  public static void main(String[] args)
+  {
+   int i=1;
+   while(i<=10)
+   {
+    System.out.println("Pranay");
+    i++;
+   }
+  }
+}
