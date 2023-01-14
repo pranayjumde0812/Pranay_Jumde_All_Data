e9e3d191777e46ab425062e21fbbf31338fd9e57
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 15:48:09 +0530

    If Else In Java

---
+++ DayNight.java
@@ -0,0 +1,14 @@
+class DayNight
+{
+  public static void main (String[] args)
+  {
+     //If Else 2
+   int time=23;
+   if(time<=19 && time>=20)
+   {
+    System.out.println("It is day time");
+
+   }else
+        System.out.println("It is night time");
+  }
+}
