7858ff4b2fac446f078106a4d05e8000e992ca4e
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 23:56:05 +0530

    Formula Problem In Java

---
+++ AreaOfTriangle.java
@@ -0,0 +1,11 @@
+class AreaOfTriangle
+{
+ public static void main(String [] args)
+ {
+  int b=23, h=10;
+
+  double area = 0.5* b* h;
+
+  System.out.println("Area of Triangle = "+area+" cm2" );
+ }
+}
