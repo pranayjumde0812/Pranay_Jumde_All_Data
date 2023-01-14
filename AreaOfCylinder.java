c3d867a6fd191ac4a273e68f4968ebf84caeb93e
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 23:55:09 +0530

    Formula Problem In Java

---
+++ AreaOfCylinder.java
@@ -0,0 +1,13 @@
+class AreaOfCylinder
+{
+ public static void main(String [] args)
+ {
+  double pi=3.14;
+  int r=8;
+  double area;
+
+  area = 4 * pi *(r*r);
+
+  System.out.println("Area of cylinder = "+area+" cm2" );
+ }
+}
