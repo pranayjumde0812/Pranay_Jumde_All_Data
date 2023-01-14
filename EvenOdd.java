c71195ba4e86a785debedbc0aab04f74ed9064ac
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-14 15:47:16 +0530

    If Else In Java

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+class EvenOdd
+{
+  public static void main (String[] args)
+  {
+	  //If Else 1
+   int num=3;
+   if(num%2==0)
+   {
+    System.out.println("The Number is Even");
+
+   }else
+        System.out.println("The Number is Odd");
+  }
+}
