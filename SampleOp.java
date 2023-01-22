642ca4531a4d03afe13280ee6e66474ed662d492
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-22 23:33:24 +0530

    19 January Ass 1
    Sample Output:
    x       x+2     x+4     x+6
    
    -------------------------------
    
    1       3       5       7
    
    4       6       8       10
    
    7       9       11      13
    
    10      12      14      16
    
    13      15      17      19

---
+++ SampleOp.java
@@ -0,0 +1,22 @@
+class SampleOp{
+
+//19 th January Assignment(Que 1)
+
+  public static void main(String[] args){
+
+
+      System.out.println("x      x+2      x+4      x+6");
+      System.out.println("===============================");
+	  for(int i=1; i<=13 ; i=i+3)
+	  {
+
+
+		  int j=i+2;
+		  int k=i+4;
+		  int l=i+6;
+
+		  System.out.print(i+ "      "+j+"         "+k+"         "+l);
+		  System.out.println();
+	  }
+  }
+}
