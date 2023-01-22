41227eaa282168f33fccb046b2a5496c8882871d
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-22 23:34:56 +0530

    19 January Ass 2
    Write a Java  program to print the result of the specified operations.
    -1 + 4 * 6
    
    ( 35+ 5 ) % 7
    
    14 + -4 * 6 / 11
    
    2 + 15 / 6 * 1 - 7 % 2

---
+++ SpecifiedOperation.java
@@ -0,0 +1,18 @@
+class SpecifiedOperation{
+
+//19 th January Assignment(Que 2)
+
+  public static void main(String[] args){
+
+   int res1=-1+4*6;
+   System.out.println(res1);
+
+   int res2=(35+5)%7;
+   System.out.println(res2);
+
+   int res3=2+15/6*1-7%2;
+
+   System.out.println(res3);
+
+  }
+}
