5531322cc9e73c6183b70f7df4c0a2f5c3f2953c
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-23 00:01:44 +0530

    18 January Ass 1
    Write a Java program that swaps two numbers without using third variable.

---
+++ SwapTwoNum.java
@@ -0,0 +1,25 @@
+ import java.util.Scanner;
+
+class SwapTwoNum
+{
+  public static void main(String [] args){
+
+  Scanner sc=new Scanner(System.in);
+  System.out.print("Enter First Number: ");
+  int x=sc.nextInt();
+  System.out.print("Enter Second Number: ");
+  int y=sc.nextInt();
+
+  System.out.println("Number before Swapping");
+  System.out.println("x= "+x);
+  System.out.println("y= "+y);
+
+   x=x+y;
+   y=x-y;
+   x=x-y;
+
+  System.out.println("Number after Swapping");
+  System.out.println("x= "+x);
+  System.out.println("y= "+y);
+  }
+}
