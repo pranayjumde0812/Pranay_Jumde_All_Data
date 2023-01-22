f601f5dff160d5fe12af26198b7fb7c935336a54
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-01-22 23:59:05 +0530

    19 January Ass 17
    Write a C program to check if two given non-negative integers have the same last digit.

---
+++ Jan_19_Que17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Jan_19_Que17
+{
+
+    public static void main(String [] args)
+    {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter any two numbers:  ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+
+        int same1= lastDigit(a);
+        int same2= lastDigit(b);
+
+        if(same1==same2)
+        {
+         System.out.println("Both numbers have Same last digits");
+     	}
+     	else
+     	 System.out.println("Both numbers dont have Same last digits");
+	}
+
+
+	  public static int lastDigit(int x)
+	  {
+        int n=x%10;
+
+           return n;
+	  }
+}
