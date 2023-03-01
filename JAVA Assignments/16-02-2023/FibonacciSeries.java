c740c00731c0a264e39d0a7bca262921e42b71a7
pranayjumde0812 <120703267+pranayjumde0812@users.noreply.github.com>
2023-03-02 00:14:45 +0530

    Fibonacci Series 1st March 2023

---
+++ JAVA Assignments/16-02-2023/FibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class FibonacciSeries
+{
+ public static void main(String [] args)
+ {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter the Number: ");
+   int n=sc.nextInt();
+
+   int n1=0, n2=1;
+   int n3;
+   System.out.print(n1+" "+n2);
+
+   for(int i=2; i<=n; i++)
+   {
+	 n3=n1+n2;
+	 System.out.print(" "+n3+" ");
+	 n1=n2;
+	 n2=n3;
+   }
+ }
+}
